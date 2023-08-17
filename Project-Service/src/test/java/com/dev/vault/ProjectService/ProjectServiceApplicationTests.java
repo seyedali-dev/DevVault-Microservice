55551841fa0b93b3e5805882687594d97cb2ddc5
SayedAli <learn.mike.helloworld@gmail.com>
2023-08-17 22:51:15 +0430

    initial commit: pom.xml files

---
+++ Project-Service/src/test/java/com/dev/vault/ProjectService/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.vault.ProjectService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
